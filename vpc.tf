6d0abd2217768d5bd5ca8891e327587ecb7f5f52
Andrew Figaroa <andrewnumber1@gmail.com>
2019-07-05 15:56:54 -0400

    create a 'final' version

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 # vpc
 module "vpc_a" {
-  source = "./vpc/"
-  subnet_vpc = "${var.subnet_vpc}"
-  subnet_dmz = "${var.subnet_dmz}"
+  source          = "git@github.com:andrewduke51/vpc.git?ref=v2.7"
+  subnet_vpc      = "${var.subnet_vpc}"
+  subnet_dmz      = "${var.subnet_dmz}"
   subnet_internal = "${var.subnet_internal}"
 }
